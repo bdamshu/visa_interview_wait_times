66307648d1e12d0233afc2766c1147280d1ed77e
Divyamsh Bosukonda <bdamshu@Divyamshs-Air.san.rr.com>
2023-01-29 14:09:54 -0800

    add comments

--- firefox_interactor.py
+++ firefox_interactor.py
@@ -14,8 +14,9 @@ b1b2_wait_times = {}
 while idx < len(city_names):
     city_name = city_names[idx]
     dropdown_menu.send_keys(city_name)
-    dropdown_menu.send_keys(Keys.ENTER)
-    sleep(2)
+    dropdown_menu.send_keys(Keys.ENTER) # works only when there 1 item in drop-down box
+    sleep(2)    # wait for wait-times to populate
+    # XML path for B1B2_interview_required wait-time field
     b1b2_wait_times[city_name] = driver.find_element_by_xpath('/html/body/div[3]/div[7]/div[2]/div[1]/div[2]/div[4]/div/div[3]/div[1]/table/tbody/tr[4]/td[2]/span').text
     
     edit_city = driver.find_element_by_class_name('edit_btn')
